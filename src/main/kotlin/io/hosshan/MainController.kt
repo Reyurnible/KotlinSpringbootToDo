e77de62a03ae7329ce979c1183edd75dc044e02e
Shun Hosaka <reyurnible@gmail.com>
2016-12-02 14:55:13 +0900

    remove open

--- src/main/kotlin/io/hosshan/MainController.kt
+++ src/main/kotlin/io/hosshan/MainController.kt
@@ -4,13 +4,14 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
 
 /**
  * Controller
  */
-@Controller
+@RestController
 @EnableAutoConfiguration
-open class MainController {
+class MainController {
     companion object {
         object Prefix {
             const val WEB = ""
@@ -21,13 +22,13 @@ open class MainController {
     // WEB
     @RequestMapping("${Prefix.WEB}/")
     @ResponseBody
-    open fun home(): String =
+    fun home(): String =
             "Hello, Spring Boot Sample Application!"
 
     // API
     @RequestMapping("${Prefix.API}/todos")
     @ResponseBody
-    open fun todos(): String =
+    fun todos(): String =
             "Hello, Spring Boot Sample Application!"
 
 
