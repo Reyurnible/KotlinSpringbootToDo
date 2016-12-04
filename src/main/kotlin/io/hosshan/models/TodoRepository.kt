0f74e474a783c75902824cba9f64358e8197561b
Shun Hosaka <reyurnible@gmail.com>
2016-12-04 15:43:01 +0900

    add comment

--- src/main/kotlin/io/hosshan/models/TodoRepository.kt
+++ src/main/kotlin/io/hosshan/models/TodoRepository.kt
@@ -1,13 +1,14 @@
 package io.hosshan.models
 
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
 
 /**
  * Repository for Todo
  */
+@Repository
 interface TodoRepository : CrudRepository<Todo, Long> {
-
 
 
 }
