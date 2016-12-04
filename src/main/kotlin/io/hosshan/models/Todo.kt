dce43c6bdf5bd2c7422a9e1997c00735fd95e9a8
Shun Hosaka <reyurnible@gmail.com>
2016-12-04 11:11:44 +0900

    add todo model

--- src/main/kotlin/io/hosshan/models/Todo.kt
+++ src/main/kotlin/io/hosshan/models/Todo.kt
@@ -1,12 +1,25 @@
 package io.hosshan.models
 
-import javax.persistence.Entity
+import java.io.Serializable
+import javax.persistence.*
 
 
 /**
  * Entity class
  */
 @Entity
-class Todo {
+@Table(name = "todos")
+class Todo(
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        @Column
+        val id: Long,
+        @Column(length = 50, nullable = false)
+        val title: String,
+        @Column(nullable = true)
+        val body: String?,
+        @Column(name = "is_done")
+        val isDone: Boolean = false
+) : Serializable {
 
 }
