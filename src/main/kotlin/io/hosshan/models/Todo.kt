ac306dec269298468efa74363fbfde257f03f066
Shun Hosaka <reyurnible@gmail.com>
2016-12-04 15:41:55 +0900

    update entity object

--- src/main/kotlin/io/hosshan/models/Todo.kt
+++ src/main/kotlin/io/hosshan/models/Todo.kt
@@ -3,7 +3,6 @@ package io.hosshan.models
 import java.io.Serializable
 import javax.persistence.*
 
-
 /**
  * Entity class
  */
@@ -15,11 +14,37 @@ class Todo(
         @Column
         val id: Long,
         @Column(length = 50, nullable = false)
-        val title: String,
+        var title: String,
         @Column(nullable = true)
-        val body: String?,
+        var body: String?,
         @Column(name = "is_done")
-        val isDone: Boolean = false
+        val isDone: Boolean? = false
 ) : Serializable {
+    constructor() : this(0, "", null)
 
+    override fun toString(): String {
+        return "Todo(id=$id, title='$title', body=$body, isDone=$isDone)"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other?.javaClass != javaClass) return false
+
+        other as Todo
+
+        if (id != other.id) return false
+        if (title != other.title) return false
+        if (body != other.body) return false
+        if (isDone != other.isDone) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + title.hashCode()
+        result = 31 * result + (body?.hashCode() ?: 0)
+        result = 31 * result + (isDone?.hashCode() ?: 0)
+        return result
+    }
 }
